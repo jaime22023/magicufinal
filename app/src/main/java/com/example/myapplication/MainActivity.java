38e8a7dbd706ef30b5bfb794bfe3a49e7bb86483
jaime <git@githut.com:jaime22023/MAGIC.git>
2020-05-15 09:48:42 -0400

    verificando, para asegurar subida de app a git

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 setContentView(R.layout.activity_main);
 
                 ball1=findViewById(R.id.boton);
-
                 respuesta=findViewById(R.id.respuesta);
 
                 ball1.setOnClickListener((View.OnClickListener) this);
